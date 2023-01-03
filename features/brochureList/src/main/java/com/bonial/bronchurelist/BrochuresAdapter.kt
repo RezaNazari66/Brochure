2fc28bc111de9e60849cbe65d4b4a4e06eb46057
Reza <nazari.reza66@gmail.com>
2023-01-03 18:27:04 +0330

    change column of brochure list

--- features/brochureList/src/main/java/com/bonial/bronchurelist/BrochuresAdapter.kt
+++ features/brochureList/src/main/java/com/bonial/bronchurelist/BrochuresAdapter.kt
@@ -7,37 +7,70 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.bonial.bronchurelist.databinding.ItemBrochureBinding
+import com.bonial.bronchurelist.databinding.ItemPremiumBrochureBinding
 import com.bonial.domain.models.Brochure
+import com.bonial.domain.models.ContentTypeEnum
 import com.bumptech.glide.Glide
 
+const val BROCHURE_VIEW_TYPE = 1
+const val BROCHURE_PREMIUM_VIEW_TYPE = 2
+
 class BrochuresAdapter(private val context: Context) :
-    ListAdapter<Brochure, BrochuresAdapter.BrochuresViewHolder>(BrochuresDiffUtils()) {
+    ListAdapter<Brochure, ViewHolder>(BrochuresDiffUtils()) {
 
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BrochuresViewHolder {
-        val binding =
-            ItemBrochureBinding.inflate(LayoutInflater.from(context), parent, false)
-        return BrochuresViewHolder(binding)
+    override fun getItemViewType(position: Int): Int {
+        val item = getItem(position)
+        return if (item.contentType == ContentTypeEnum.BROCHURE_PREMIUM) BROCHURE_PREMIUM_VIEW_TYPE else BROCHURE_VIEW_TYPE
     }
 
-    override fun onBindViewHolder(holder: BrochuresViewHolder, position: Int) {
-        val item = getItem(position)
-        holder.binding.apply {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return if (viewType == BROCHURE_VIEW_TYPE) {
+            val binding = ItemBrochureBinding.inflate(LayoutInflater.from(context), parent, false)
+            BrochuresViewHolder(binding)
+        } else {
+            val binding =
+                ItemPremiumBrochureBinding.inflate(LayoutInflater.from(context), parent, false)
+            BrochuresPremiumViewHolder(binding)
+        }
+    }
 
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+
+        if (item.contentType == ContentTypeEnum.BROCHURE_PREMIUM) {
+            (holder as BrochuresPremiumViewHolder).bind(item)
+        } else {
+            (holder as BrochuresViewHolder).bind(item)
+        }
+
+    }
+
+    inner class BrochuresViewHolder(private val binding: ItemBrochureBinding) :
+        ViewHolder(binding.root) {
+        fun bind(item: Brochure) {
             Glide
                 .with(context)
                 .load(item.image)
                 .placeholder(R.drawable.placeholder)
                 .fitCenter()
-                .into(this.imageView)
-
-            TitleTextView.text = item.retailerName
+                .into(binding.imageView)
+            binding.titleTextView.text = item.retailerName
         }
-
     }
 
-
-    class BrochuresViewHolder(val binding: ItemBrochureBinding) : ViewHolder(binding.root)
+    inner class BrochuresPremiumViewHolder(private val binding: ItemPremiumBrochureBinding) :
+        ViewHolder(binding.root) {
+        fun bind(item: Brochure) {
+            Glide
+                .with(context)
+                .load(item.image)
+                .placeholder(R.drawable.placeholder)
+                .centerCrop()
+                .into(binding.imageView)
+            binding.titleTextView.text = item.retailerName
+        }
+    }
 
     class BrochuresDiffUtils : DiffUtil.ItemCallback<Brochure>() {
         override fun areItemsTheSame(oldItem: Brochure, newItem: Brochure): Boolean {
